1e6abe43647ecacebcea682ca5cf844e52ad803b
anshthakur56854-cpu <anshthakur56854-cpu@gmail.com>
2026-07-27 20:48:50 +0530

    Added Python programs

---
+++ Decision control.py
@@ -0,0 +1,132 @@
+# IF ,else ,elif
+# # DICISION CONTROL   CONDITION H 
+# #   1. IF ELSE STATEMENT 
+
+# # SYNTEX:
+ 
+# if condition:
+
+#     #code to execute if condition is TRUE
+
+#     else
+
+#      #code to execute if conditon is FALSE 
+      
+
+
+# # ELIF STATMENT 
+
+# #SYNTEX:
+
+# if condition 1:
+
+# #code
+
+# elif condition 2:
+# # code
+
+# elif condition 3:
+
+# elif condition 4:
+# # code
+# else:
+#     # code is execute  if all condition are FALSE 
+#  input("Enter mrp of book  and 10% discaunt")
+
+#   if mrp is >= 1000 otherwise  5% discaunt
+
+
+# mrp =float(input("Enter mrp of book: "))
+
+# discount = 0
+# if mrp>= 1000:
+#  discount = mrp*10/100
+
+# else:
+#    discount =mrp*5/100
+
+
+#    net = mrp-discount 
+
+
+
+#    print(f"mrp ={mrp} and discount = {discount} and net ={net}")
+
+
+
+
+
+
+# #ENERT THE SALE PRICE AND COST PRICE OF PRODUCT AND FINDE
+# #  THE PROFIT OR LOSS
+
+
+# cp = float(input("Enter the cost price: "))
+
+# sp = float(input("Enter the sale price: "))
+
+# if  sp > cp:
+#     profit =sp - cp
+#     print("profit = " , profit)
+
+# elif sp < cp:
+
+#    loss = cp - sp 
+#    print ("Loss =" ,loss)
+
+# else :
+
+#     print("No  profit No loss ")
+   
+
+
+ #ENTER 2 NO. AND PRINT WHICH ONE IS GRAETER 
+
+
+# num1 = float(input("Enter the first no. = "))
+
+# num2  = float(input("Enter the  second no. = "))
+
+# if num1 > num2 :
+
+#    print("num1 = is greater")
+
+# elif num1 < num2 :
+
+#    print("num2 = is greater")
+
+# else :
+#    print("num1 and num2 is Equal")
+
+
+
+# #ENTER THE NUMBER  CHECK NO. IS ODD OR EVEN:
+
+# num = int(input("Enter a number : "))
+
+# if num % 2 == 0:
+
+#     print("Even number ")
+
+# else :
+
+#     print("Odd number ")
+   
+
+# ENTER THE NUMBER  CHECK IS POSITIVE OR NEGITIVE :
+
+
+# num  = float(input("Enter a number : "))
+
+# if num > 0:
+#      print("positive number :")
+
+
+# elif num < 0:
+#      print("Negitive number :")
+
+# else :
+#      print("Zero:")
+
+
+
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+F MR IS >= 1000 OTHERWISE 5% DISCAUN
+
+
+mrp float(input"Enter mrp of book: ")
+
+discaunt = 0
+if mrp>= 10
